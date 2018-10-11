4eab4d762fbdd7e760bd7cc3f337c6332db4ddb2
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-11 16:09:06 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,29 +7,7 @@ class Book
     @title
   end
 
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(pages)
-    @page_count = pages
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
-  end
+  attr_accessor :author, :page_count, :genre
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
