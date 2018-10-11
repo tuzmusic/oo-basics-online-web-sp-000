25a1146318daa3457701614797cc10232f21f596
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-11 16:05:44 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,4 +2,18 @@ class Book
   def initialize(title)
     @title = title
   end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+
 end
